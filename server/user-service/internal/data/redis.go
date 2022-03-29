3e900e744c0b67c7440b13d5d584e2e9feea0774
CoolSnow <coolsnow2020@gmail.com>
2022-03-29 19:11:49 +0800

    Update redis.go

--- server/user-service/internal/data/redis.go
+++ server/user-service/internal/data/redis.go
@@ -1,1 +1,19 @@
 package data
+
+import (
+	"context"
+	"strconv"
+	"time"
+)
+
+const lastCreateUserID = "lastCreateUserID"
+
+func (ur *userRepo) GetLastCreateUserID(ctx context.Context) (id int64, err error) {
+	ret := ur.data.rdb.Get(ctx, lastCreateUserID)
+	return strconv.ParseInt(ret.Val(), 10, 64)
+}
+
+func (ur *userRepo) SetLastCreateUserID(ctx context.Context, id int64) (err error) {
+	ret := ur.data.rdb.Set(ctx, lastCreateUserID, id, time.Millisecond*200)
+	return ret.Err()
+}
